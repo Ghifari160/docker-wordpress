7bf47f04fe5fd2e56d27d6caae564f1e05fd1930
Ghifari160 <ghifari160@ghifari160.com>
2018-06-11 19:25:59 -0500

    Update deprecated instructions and update init script
    MAINTAINER instructions are replaced by LABEL instructions. Init script
    follows ghifari160/apache's init script, though it adds installation
    procedure for Wordpress.
    
    - Replace MAINTAINER instructions with LABEL MAINTAINER in
      16.04/Dockerfile and 17.10/Dockerfile
    - Rename init script in the container to /etc/g16/wp_init.sh to avoid
      overwriting ghifari160/apache's init.sh in 16.04/Dockerfile and
      17.10/Dockerfile
    - Change ENTRYPOINT to /etc/g16/wp_init.sh in 16.04/Dockerfile and
      17.10/Dockerfile
    - Use ghifari160/apache's init.sh as the basis of wp_init.sh in
      16.04/init.sh and 17.10/init.sh

--- 17.10/init.sh
+++ 17.10/init.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
 
+set -e
+set -m
+
+function macOS_fixPerms()
+{
+  if [ ! -f "/etc/g16/apache_macOS_fix" ]; then
+    echo "=============================="
+    echo "=  Fixing macOS Permissions  ="
+    echo "=============================="
+
+    # Reassign UID and GID in macOS
+    usermod -u 1000 www-data
+    groupdel staff
+    groupmod -g 50 www-data
+
+    # Reconfigure file permissions
+    chown -fR www-data:www-data /
+
+    # Store g16 metadata
+    mkdir -p /etc/g16
+    touch /etc/g16/apache_macOS_fix
+    echo "true" > /etc/apache_macOS_fix
+  fi
+}
+
 function wordpress_install()
 {
+  echo "=============================="
+  echo "=    Installing Wordpress    ="
+  echo "=============================="
+
   cd /var/www && tar -xvzf /tmp/g16/wp.tar.gz
-  # rm -rf /var/www/html
-  # mv /var/www/wordpress /var/www/html
   cp -a /var/www/wordpress/. /var/www/html/
   rm -rf /var/www/wordpress
 }
 
-function wordpress_fixperms()
-{
-  chown -fR www-data:www-data /var/www/html
-  chmod -fR 777 /var/www/html
-}
+if [ "$G16_MACOS" = "yes" ]; then
+  macOS_fixPerms
+fi
 
 if [ ! -d "/var/www/html/wp-admin" ]; then
   wordpress_install
-  wordpress_fixperms
 fi
 
-/usr/sbin/apachectl -D FOREGROUND
+echo "=============================="
+echo "=      Staring Apache2       ="
+echo "=============================="
+exec /usr/sbin/apachectl -D FOREGROUND
+echo "=============================="
+echo "=        Ready to Use!       ="
+echo "=============================="
+
+fg
